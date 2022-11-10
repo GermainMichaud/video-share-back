75a5f42f192df4711f54526a1f24c6b590763bd3
Germain Michaud <gmichaud.dev@gmail.com>
2022-11-10 04:27:29 +0100

    feat: add all routes to the app

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,19 @@
-import { Application } from 'express';
+import { Application, Router } from 'express';
 
 import healthcheckRoutes from './healthcheck.routes';
+import oauthRoutes from './oauth.routes';
+import userRoutes from './user.routes';
+import videoRoutes from './video.routes';
 
 const loadRoutes = (app: Application) => {
-  app.use('/api', [healthcheckRoutes]);
+  const router = Router();
+
+  router.use('/healthcheck', healthcheckRoutes);
+  router.use('/oauth', oauthRoutes);
+  router.use('/video', videoRoutes);
+  router.use('/user', userRoutes);
+
+  app.use('/api/', router);
 };
 
 export default loadRoutes;
