cb7b1aea0ee8fc31c74684500e328225eb4cdb90
Germain Michaud <gmichaud.dev@gmail.com>
2022-11-10 04:12:53 +0100

    feat: update config

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -1,3 +1,8 @@
 export default {
   port: 'PORT',
+  redirect_uri: 'REDIRECT_URI',
+  tiktok: {
+    client_key: 'TIKTOK_CLIENT_KEY',
+    client_secret: 'TIKTOK_CLIENT_SECRET',
+  },
 };
--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,11 @@
 export default {
   port: 4000,
+  redirect_uri: '',
+  tiktok: {
+    client_key: '',
+    client_secret: '',
+  },
+  session: {
+    secret: 'secret',
+  },
 };
