c4524337cb74e2abd44e6116cb655ec8bcce8dec
Germain Michaud <gmichaud.dev@gmail.com>
2022-11-10 04:18:44 +0100

    feat: add express-session & cookie-parser to express middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,11 +1,32 @@
 import compression from 'compression';
+import config from 'config';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
+import session from 'express-session';
 import helmet from 'helmet';
 
 const loadMiddlewares = (app: Application) => {
   app.use(helmet());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+    }),
+  );
+  app.use(
+    session({
+      secret: config.get<string>('session.secret'),
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(compression());
